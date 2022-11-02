7c966c08f507db8cacbe2549de35b87f6db03035
fred231084g <freddy1984govoni@gmail.com>
2022-11-02 22:48:49 +0100

    Delete common-lib directory

--- common-lib/test/cmr/common/test/api/errors.clj
+++
@@ -1,9 +0,0 @@
-(ns cmr.common.test.api.errors
-  "This tests capabilities of the API error utilities."
-  (:require
-   [clojure.test :refer :all]
-   [cmr.common.api.errors :as errors]))
-
-(deftest test-error-masked
-  (let [error-message "Token 123 does not exist"]
-    (is (= "Token does not exist" (errors/mask-token-error error-message)))))
