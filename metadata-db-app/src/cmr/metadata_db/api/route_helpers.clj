cec6d178d043001da0cf8f8c52f92a0f47e66aa3
fred231084g <freddy1984govoni@gmail.com>
2022-11-02 22:49:22 +0100

    Delete metadata-db-app directory

--- metadata-db-app/src/cmr/metadata_db/api/route_helpers.clj
+++
@@ -1,6 +0,0 @@
-(ns cmr.metadata-db.api.route-helpers
-  (:require
-   [cmr.common.mime-types :as mt]))
-
-(def json-header
-  {"Content-Type" (mt/with-utf-8 mt/json)})
--- metadata-db-app/support/drop_user.clj
+++
@@ -1,7 +0,0 @@
-;;; run with lein exec -p create_user.clj
-(require '[cmr.db :as db])
-
-(try
-  (db/drop-user)
-  (finally
-    (shutdown-agents)))
